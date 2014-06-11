1f84fb1dae513510d6608606e7f57380953fd713
KentHuang <ubuntu@ip-172-31-8-137.us-west-2.compute.internal>
2014-06-11 05:26:43 +0000

    stuff

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+var fs = require('fs')
 var express = require('express')
 var app = express();
 
+var str = fs.readFileSync('index.html').toString()
+ 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World hows it going!')
+ // response.send('Hello World hows it going!')
+    response.send(str)
 })
 
 app.listen(app.get('port'), function() {
