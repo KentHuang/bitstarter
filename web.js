ce8979f3eb7bf88db68df3cc4725ade79b8fb587
KentHuang <ubuntu@ip-172-31-8-137.us-west-2.compute.internal>
2014-06-11 05:06:13 +0000

    testing web.js again'

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express')
+var app = express();
 
-var app = express.createServer(express.logger());
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (request, response) {
-    response.send('Hi World!');
-});
+app.get('/', function(request, response) {
+  response.send('Hello World hows it going!')
+})
 
-var port = process.env.PORT || 5000;
-
-app.listen(port, function() {
-    console.log("listening on " + port);
-});
-
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
